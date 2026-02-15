91efd4fc9406fb874b22971c8f7f69b52dbb1c57
bububak <marek.mraz@outlook.com>
2026-02-15 19:27:52 +0100

    updated game drawing to use custom module

--- svl/battleships/battleships.py
+++ svl/battleships/battleships.py
@@ -1,8 +1,18 @@
+from blessings import set_char, clear_screen, hide_cursor
+
+
 class Tile():
 
     character_dict = {
         ".":"blank",
-        "~":"water"
+        "~":"water",
+        "<":"left",
+        ">":"right",
+        "A":"up",
+        "V":"down",
+        "#":"full",
+        "o":"circle",
+        "?":"ship"
     }
 
     def __init__(self,x,y,c) -> None:
@@ -14,7 +24,12 @@ class Tile():
 
 class Bot():
     def __init__(self) -> None:
-        pass
+        self.should_run = True
+
+
+    def start_logic(self):
+        while self.should_run:
+            pass
 
 
 
@@ -31,7 +46,6 @@ class Game():
         self.y_values = f.readline().strip().split()
         self.size_x = len(self.x_values)
         self.size_y = len(self.y_values)
-        print(self.x_values,self.y_values)
 
         self.field = []
         temp_line = []
@@ -43,18 +57,32 @@ class Game():
             self.field.append(temp_line.copy())
             temp_line = []
         self.field.append(["." for n in range(self.size_x+1)])
+        f.close()
 
         for y in range(len(self.field)):
             for x in range(len(self.field[y])):
                 self.field[y][x] = Tile(x,y,self.field[y][x])
 
+
     def draw_field(self):
-        for y in range(self.size_y):
-            for x in range(self.size_x):
-                print(self.field[y][x].c,end="")
-            print()
+        clear_screen()
+        first_row = " |"
+        second_row = "-+"
+        for i in self.x_values:
+            first_row += i
+            second_row += "-"
+        set_char(0,0,first_row)
+        set_char(0,1,second_row)
+        for i in range(len(self.y_values)):
+            current_row = f"{self.y_values[i]}|"
+            for j in range(len(self.x_values)):
+                current_row += self.field[i][j].c
+            set_char(0,i+2,current_row)
+        hide_cursor()
 
 
 
 game = Game()
+bot = Bot()
 game.draw_field()
+bot.start_logic()
