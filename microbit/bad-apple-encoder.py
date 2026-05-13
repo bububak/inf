f3df02c2048f9f040a63a9f5eed33fc035548896
bububak <marek.mraz@outlook.com>
2026-05-13 12:42:25 +0200

    bad-apple-encoding update
    all frames are now 5x5 numpy arrays with light values

--- microbit/bad-apple-encoder.py
+++ microbit/bad-apple-encoder.py
@@ -22,4 +22,43 @@ def base_convert(original, base_input, base_output):
 
 
 
-print(cv2.getBuildInformation())
+TARGET_FRAMERATE = 30
+TARGET_W, TARGET_H = 5, 5
+INPUT_VIDEO = "badapple.mp4"
+OUTPUT_FILE = "badapple.txt"
+
+
+cap = cv2.VideoCapture(INPUT_VIDEO)
+source_fps = cap.get(cv2.CAP_PROP_FPS)
+frame_skip_amount = source_fps // TARGET_FRAMERATE
+
+
+
+ret, frame = cap.read()
+W, H = frame.shape[:2]
+size = min(W, H)
+x0 = (W - size) // 2
+y0 = (H - size) // 2
+
+framei = 0
+
+while ret:
+
+    # only process every x frames
+    if framei % frame_skip_amount == 0:
+
+        print(framei)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        square = gray[y0:y0+size, x0:x0+size]
+        small = cv2.resize(square, (TARGET_W, TARGET_H), interpolation=cv2.INTER_AREA)
+        print(small)
+
+
+    framei += 1
+    ret, frame = cap.read()
+
+
+
+cap.release()
+cv2.destroyAllWindows()
