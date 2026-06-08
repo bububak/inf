72c71bf0b6aaea8ea29f69e1bcf2ac0f018f7f9b
bububak <marek.mraz@outlook.com>
2026-06-08 09:27:01 +0200

    working splitters and looping prevention

--- svl/zrkadla/main.py
+++ svl/zrkadla/main.py
@@ -11,11 +11,6 @@ class Ray:
     def tick(self):
         global field, visited
 
-        # add coordinate to visited before moving - does not write outside play area
-        visited[self.y][self.x][0 if (self.dir == RIGHT or self.dir == LEFT) else 1] = (
-            True
-        )
-
         # move 1 space
         self.x, self.y = self.x + self.dir[0], self.y + self.dir[1]
 
@@ -29,12 +24,25 @@ class Ray:
             rays.pop()
             return
 
+        # delete self if space traversed
+        if ((self.dir == RIGHT or self.dir == LEFT) and visited[self.y][self.x][0]) or (
+            (self.dir == UP or self.dir == DOWN) and visited[self.y][self.x][1]
+        ):
+            rays.pop()
+            return
+
+        # add coordinate to visited before moving - does not write outside play area
+        visited[self.y][self.x][0 if (self.dir == RIGHT or self.dir == LEFT) else 1] = (
+            True
+        )
+        print(self.x, self.y, field[self.y][self.x], self.dir, self)
+
         # direction change on special tiles
         space = field[self.y][self.x]
 
-        if space in "|-":  # splitter
-            pass
-            self.dir = directions["/"][directions["/"].index(self.dir) - 2]
+        if (space == "|" and (self.dir == RIGHT or self.dir == LEFT)) or (
+            space == "-" and (self.dir == UP or self.dir == DOWN)
+        ):  # splitter
             rays.append(
                 Ray(
                     self.x,
@@ -42,36 +50,43 @@ class Ray:
                     directions["\\"][directions["\\"].index(self.dir) - 2],
                 )
             )
+            self.dir = directions["/"][directions["/"].index(self.dir) - 2]
 
-        if space in "\\/":
+        elif space in "\\/":
             self.dir = directions[space][directions[space].index(self.dir) - 2]
 
 
 # mainloop
 def mainloop(stdscr):
-    while rays:
+    while len(rays) > 0:
         rays[-1].tick()
         sleep(0.01)
 
+
+# load file into field
+with open("zrkadla-custom.txt", "r") as f:
+    field = f.read().split("\n")
+    field.pop()
+# visited: mapa s [T/F, T/F] na kazdom mieste v poradi "navstivene horizontalne", "navstivene vertikalne"
+visited = [[[False, False] for x in range(len(field[0]))] for y in range(len(field))]
 
 # directions as (x,y)
 UP, DOWN, RIGHT, LEFT = (0, -1), (0, 1), (1, 0), (-1, 0)
 directions = {"/": (UP, DOWN, RIGHT, LEFT), "\\": (UP, DOWN, LEFT, RIGHT)}
 rays = [
-    Ray(0, 0, RIGHT),
+    Ray(-1, 0, RIGHT),
 ]
 
-# load file into field
-with open("zrkadla-nosplit.txt", "r") as f:
-    field = f.read().split("\n")
-# visited: mapa s [T/F, T/F] na kazdom mieste v poradi "navstivene horizontalne", "navstivene vertikalne"
-visited = [[[False, False] for x in range(len(field[0]))] for y in range(len(field))]
-
-
 # code to execute
-curses.wrapper(mainloop)
+SHOULD_ANIMATE = False
+if SHOULD_ANIMATE:
+    curses.wrapper(mainloop)
+else:
+    mainloop("a")
 print("finished")
 
+
+# count visited
 n = 0
 for row in range(len(visited)):
     for column in range(len(visited[0])):
