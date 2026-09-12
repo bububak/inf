bb93407c944e0f877bacd27f17f95c95028df488
bubak <marek.mraz@outlook.com>
2026-09-12 16:58:01 +0200

    use pictures instead of numbers on canvas with as little code changes

---
+++ hodina/zabky/usporiadacia_hra_obrazky.py
@@ -0,0 +1,96 @@
+import random
+import tkinter as tk
+
+
+def vypis_poradie():
+    pictures = {}
+    for i,n in enumerate(poradie):
+        current_image = tk.PhotoImage(file=f"zabka{n}.png")
+        pictures[n] = current_image
+        canvas_items[n] = c.create_image(SQUARE_SIZE * i + SQUARE_SIZE // 2, SQUARE_SIZE // 2, image=current_image)
+    c.update()
+    return pictures
+
+def check_sorted():
+    p = poradie.copy()
+    if p[0] != "_" and p[-1] != "_":
+        return False
+    p.remove("_")
+    return p == sorted(p)
+
+
+def is_valid_input(num: str):
+    if not num.isnumeric():
+        vypis_error("input musi byt cislo")
+        return False
+    if int(num) > LIST_LENGTH or int(num) < 1:
+        vypis_error(f"input musi byt v rozsahu 1 - {LIST_LENGTH}")
+        return False
+    if abs(poradie.index("_") - poradie.index(int(num))) > EXCHANGE_RADIUS:
+        vypis_error("zadane cislo je pridaleko od _")
+        return False
+    return True
+
+
+def get_user_input(e) -> None:
+    global turn_counter
+    x = e.x // SQUARE_SIZE
+
+    if x < 0 or x > LIST_LENGTH:
+        return
+
+    clear_label()
+    n = poradie[x]
+    if not is_valid_input(str(n)):
+        return
+
+    swap_canvas_items(x, poradie.index("_"))
+    turn_counter += 1
+
+    if check_sorted():
+        feedback_label.config(text=f"Vyhral si na {turn_counter} tahov!", font=("Arial", LABEL_FONT_SIZE, "bold"), fg=colors["green"])
+        c.unbind("<1>")
+
+
+def swap_canvas_items(pos1, pos2):
+    a = poradie[pos1]
+    b = poradie[pos2]
+    poradie[pos1], poradie[pos2] = b, a
+    c.itemconfig(canvas_items[a], image=pictures[b])
+    c.itemconfig(canvas_items[b], image=pictures[a])
+    canvas_items[a], canvas_items[b] = canvas_items[b], canvas_items[a]
+    c.update()
+
+
+def vypis_error(message: str):
+    feedback_label.config(text=message, fg=colors["red"])
+
+
+def clear_label():
+    feedback_label.config(text="")
+
+
+colors = {"text":"#cdd6f4", "bg":"#1e1e2e","red":"#f38ba8", "green":"#a6e3a1", "blue":"#89b4fa"}
+
+LIST_LENGTH = 6
+EXCHANGE_RADIUS = 2
+canvas_items = {}
+turn_counter = 0
+poradie = [n + 1 for n in range(LIST_LENGTH)] + ["_"]
+while check_sorted():
+    random.shuffle(poradie)
+
+SQUARE_SIZE = 230
+LABEL_FONT_SIZE = 20
+root = tk.Tk()
+root.configure(bg=colors["bg"])
+feedback_label = tk.Label(root, text=f"Zorad cisla 1-{LIST_LENGTH} vymienanim s \"_\"", font=("Arial", LABEL_FONT_SIZE, "bold"),fg=colors["blue"], bg=colors["bg"])
+feedback_label.pack(padx=10, pady=10)
+c = tk.Canvas(root, width=SQUARE_SIZE * LIST_LENGTH + SQUARE_SIZE, height=SQUARE_SIZE, bg=colors["bg"], highlightthickness=0)
+c.pack()
+
+
+
+pictures = vypis_poradie()
+_ = c.bind("<1>", get_user_input)
+c.mainloop()
